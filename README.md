3fb09a924c4a644fb153f6ce31f3e78a45bc37d1
zhangwenbin <942623159@qq.com>
2019-03-27 17:38:41 +0800

    'del'

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # webpack-base-front
 
 ### webpack配置
+
+* 开发依赖包
+
+        webpack 基础工具    
+        webpack-dev-server 本地开发服务   
+        webpack 打包依赖库
+    
+* 文件目录说明
+
+        build  webpack配置
+        dist   打包输出
+        src    项目主目录
--- src/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-import 'assets/sass/reset.scss'
-import logo from 'assets/images/logo.png'
-(function() {
-  var ele = document.createElement('div');
-  ele.innerHTML = 'wqeqweqweqwesad';
-  document.body.appendChild(ele);
-})();
